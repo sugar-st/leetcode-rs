317941e9e4e729870605f578bca2dcaa59400c73
garasu <nickko@yeah.net>
2022-10-09 22:55:05 +0800

    add 2027

--- src/easy2.rs
+++ src/easy2.rs
@@ -430,3 +430,19 @@ pub fn min_time_to_type(word: String) -> i32 {
     }
     res
 }
+// 2027: https://leetcode.cn/problems/minimum-moves-to-convert-string/
+pub fn minimum_moves(s: String) -> i32 {
+    let s = s.as_bytes();
+    let mut i = 0;
+    let mut res = 0;
+    while i < s.len() {
+        if s[i] == b'X' {
+            res += 1;
+            i += 3;
+        } else {
+            i += 1;
+        }
+        
+    }
+    res
+}
