7204a4b1f8e2045f43875f716a312955af8a5f30
garasu <nickko@yeah.net>
2022-10-09 10:46:22 +0800

    simplify 455

--- src/easy1.rs
+++ src/easy1.rs
@@ -609,21 +609,17 @@ pub fn min_moves_to_equal_array_elements_ii(nums: Vec<i32>) -> i32 {
     nums.iter().sum::<i32>() - nums.iter().min().unwrap() * (nums.len() as i32)
 }
 // 455: https://leetcode-cn.com/problems/assign-cookies/
-pub fn find_content_children(g: Vec<i32>, s: Vec<i32>) -> i32 {
-    let mut g = g;
-    let mut s = s;
+pub fn find_content_children(mut g: Vec<i32>, mut s: Vec<i32>) -> i32 {
     s.sort();
     g.sort();
     let mut res = 0;
-    let mut i = 0;
-    for cookie in s {
-        if i >= g.len() {
-            break;
-        }
-        if g[i] <= cookie {
+    let (mut i, mut j) = (0, 0);
+    while i < g.len() && j < s.len() {
+        if g[i] <= s[j] {
             res += 1;
-            i += 1;
+            i += 1; 
         }
+        j += 1;
     }
     res
 }
